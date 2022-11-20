a91f33c8a678f05e35199da12a12669e1777639f
GudikandulaPravallika <21bq1a4250@vvit.net>
2022-11-20 20:40:15 +0530

    Modified

--- APLab16.py
+++ APLab16.py
@@ -1,3 +1,7 @@
-import Image
-image = Image.open('RadhaKrishna.jpg')
-image.show()
+import cv2 as cv
+import matplotlib.pyplot as pp
+import sys
+img = cv.imread(r"C:\Users\miriy\OneDrive\Pictures\Camera Roll\WIN_20221120_19_54_25_Pro.jpg",1)
+if img is None:
+ sys.exit("Could not read the image.")
+cv.imshow(r"Image",img)
